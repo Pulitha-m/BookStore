8ed5f326ed77d9e96dc431b9cdaa0006ccec34ef
Pulitha Marasinghe <pulithamarasinghe@gmial.com>
2024-12-03 01:43:14 +0530

    Added front end react for Read books and Insert books

--- store-frontend/src/App.jsx
+++ store-frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css';
+import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './layout/NavBar';
+import Navbar from "./layout/NavBar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddBook from "./Book/AddBook";
 
 function App() {
- 
-
   return (
-   <Navbar/>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/addbook" element={<AddBook />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
-export default App
+export default App;
--- store-frontend/src/layout/NavBar.jsx
+++ store-frontend/src/layout/NavBar.jsx
@@ -1,25 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">FullStack Application</a>
-          <ul className="navbar-nav ms-auto"> {/* Align items to the right */}
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
+          <a className="navbar-brand" href="#">
+            Book Store
+          </a>
+          <Link className="btn btn-light ms-auto" to="/addbook">
+            Add Book
+          </Link>
         </div>
       </nav>
     </div>
