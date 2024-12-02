32c2f6771858272ec4fd798b041224bdd32aeb14
Pulitha Marasinghe <pulithamarasinghe@gmial.com>
2024-12-03 00:10:41 +0530

    Added navbar

---
+++ store-frontend/src/layout/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">FullStack Application</a>
+          <ul className="navbar-nav ms-auto"> {/* Align items to the right */}
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
